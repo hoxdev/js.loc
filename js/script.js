8b2201df3f2d7f50e6e3495ca866b360a54fa0d7
Roman Akhmetyanov <vevogm@gmail.com>
2018-10-15 23:48:07 +0600

    Use js elements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-/* поиск эелементов по id getElementById */
+/* поиск эелементов по id getElementById
 var element = document.getElementById('box');
-console.log(element);
+поиск css селекторов querySelector , all всех
+ */
+var element2 = document.querySelectorAll('.menu-item a');
+var link = element2[2];
+
+element2[2].innerHTML = '<strong>New text</strong>';
+/*link.classList.add('js-ups');*/
+/*link.style.backgroundColor = 'green';*/
+/*link.style.fontSize = '24px';*/
+
+
+console.log(element2);
