ca6d344952348914fed2d605d53d1c2234229835
Roman Akhmetyanov <vevogm@gmail.com>
2018-10-16 22:49:42 +0600

    Add div element

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 /* поиск эелементов по id getElementById
 var element = document.getElementById('box');
 поиск css селекторов querySelector , all всех
- */
+
 var element2 = document.querySelectorAll('.menu-item a');
 var link = element2[2];
 
 element2[2].innerHTML = '<strong>New text</strong>';
-/*link.classList.add('js-ups');*/
+/*link.classList.add('js-ups');
 /*link.style.backgroundColor = 'green';*/
 /*link.style.fontSize = '24px';*/
 
-
-console.log(element2);
+/* обавить новый элемент на страницу*/
+var element = document.createElement('div');
+element.classList.add('box');
+element.innerHTML = 'some text';
+var wrapper = document.querySelector('.wrapper');
+wrapper.appendChild(element);
